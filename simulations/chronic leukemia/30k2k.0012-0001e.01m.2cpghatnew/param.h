bdcca9d0c014756d90c59148ecb307e5b3c34f31
CalumGabbutt <c.gabbutt@qmul.ac.uk>
2021-02-05 16:37:29 +0000

    Added simulation results from paper

---
+++ simulations/chronic leukemia/30k2k.0012-0001e.01m.2cpghatnew/param.h
@@ -0,0 +1,39 @@
+[Number of Cells]
+N=30000
+
+[Number of Transitions] 
+T=2000
+
+[CpG Site Flip Rate Behavior - 0 for Exponential, 1 for Manual Entry]
+B=1
+
+[CpG Site Flip Rate Lower Bound] 
+SMin=0.0005
+
+[CpG Site Flip Rate Upper Bound] 
+SMax=0.001
+
+[Random Replacement Rate]
+R=0
+
+[Orderly Replacement Rate]
+OR=0
+
+[Neoplasia Cycle]
+X=1
+
+[Expansion Rate]
+E=0.01
+
+[Max Expansion Proportion]
+M=0.2
+
+[Random Seed - Use "-1" if you want to seed using time]
+A=11
+
+[Statistics Return Regularity - Use "-1" if you just want results at the end]
+P=50
+
+init_table_fp=cpg.csv
+matrix_output_fp=results/matrix
+stats_output_fp=results/stats
